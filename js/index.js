65f6338ee055bc3cabc4bea8006f3db832d38983
guiazevedo17 <gui.azevedo@ymail.com>
2022-10-25 18:01:22 -0300

    telas de cadastro ok

--- js/index.js
+++ js/index.js
@@ -21,6 +21,9 @@ const cliente = document.querySelector('.cliente')
 const buttonCatador = document.querySelector('.popup-catador')
 const catador = document.querySelector('.catador')
 
+const buttonContinueCatador = document.querySelector('.continue-catador')
+const catador2 = document.querySelector('.catador2')
+
 
 buttonNewAccount.addEventListener('click', () => {
     popup.style.display = 'block'
@@ -37,17 +40,35 @@ buttonCatador.addEventListener('click', () => {
     catador.style.display = 'block'
 })
 
+buttonContinueCatador.addEventListener('click', () => {
+    catador.style.display = 'none'
+    catador2.style.display = 'block'
+})
+
 
 popup.addEventListener('click', event => {
     const classNameOfClickedElement = event.target.classList[0]
     const classNames = ['popup-close', 'popup-link', 'popup-wrapper']
     const shouldClosePopup = classNames.some(className => className === classNameOfClickedElement)
+    const shouldReturnPopupContas = 'popup-return' === classNameOfClickedElement
+    const shouldReturnPopupCatador = 'popup-return-catador2' === classNameOfClickedElement
 
     if(shouldClosePopup){
 
         popup.style.display = 'none'
         cliente.style.display = 'none'
         catador.style.display = 'none'
+
+    } else if(shouldReturnPopupContas) {
+
+        contas.style.display = 'block'
+        cliente.style.display = 'none'
+        catador.style.display = 'none'
+
+    } else if(shouldReturnPopupCatador){
+
+        catador2.style.display = 'none'
+        catador.style.display = 'block'
 
     }
 })
