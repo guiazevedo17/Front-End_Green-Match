09b62141669fa6bf59971902d6f9caf4c342551e
guiazevedo17 <gui.azevedo@ymail.com>
2022-11-17 23:35:34 -0300

    inicio integracao com o banco

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
-function logIn(){
+function logIn() {
     window.location = "client.html";
 }
 
-function newCollect(){
+function newCollect() {
     window.location = "newCollect.html";
 }
 
-function chooseAddress(){
+function chooseAddress() {
     window.location = "chooseAddress.html";
 }
 
@@ -53,22 +53,77 @@ popup.addEventListener('click', event => {
     const shouldReturnPopupContas = 'popup-return' === classNameOfClickedElement
     const shouldReturnPopupCatador = 'popup-return-catador2' === classNameOfClickedElement
 
-    if(shouldClosePopup){
+    if (shouldClosePopup) {
 
         popup.style.display = 'none'
         cliente.style.display = 'none'
         catador.style.display = 'none'
 
-    } else if(shouldReturnPopupContas) {
+    } else if (shouldReturnPopupContas) {
 
         contas.style.display = 'block'
         cliente.style.display = 'none'
         catador.style.display = 'none'
 
-    } else if(shouldReturnPopupCatador){
+    } else if (shouldReturnPopupCatador) {
 
         catador2.style.display = 'none'
         catador.style.display = 'block'
 
     }
+})
+
+// Integração com o BACK-END
+const form = document.querySelector("form");
+
+const Iname = document.querySelector(".name");
+const IlastName = document.querySelector(".lastName");
+const IuserName = document.querySelector(".userName");
+const Itel = document.querySelector(".tel");
+const Iemail = document.querySelector(".email");
+const IemailConf = document.querySelector(".emailConf");
+const Ipassword = document.querySelector(".password");
+const IpasswordConf = document.querySelector(".passwordConf");
+const IbirthDay = document.querySelector(".birthDay");
+
+function register() {
+
+    fetch("http://localhost:8080/usuarios",
+        {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            method: "POST",
+            body: JSON.stringify({
+                name: Iname.value + IlastName.value,
+                username: IuserName.value,
+                phone: Itel.value,
+                email: Iemail.value,
+                password: Ipassword.value,
+                birthday: IbirthDay.value
+            })
+        })
+        .then(function (res) { console.log(res) })
+        .then(function (res) { console.log(res) })
+
+};
+
+function clear() {
+    Iname.value = "";
+    IlastName.value = "";
+    IuserName.value = "";
+    Itel.value = "";
+    Iemail.value = "";
+    IemailConf.value = "";
+    Ipassword.value = "";
+    IpasswordConf.value = "";
+    IbirthDay.value = "";
+};
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    register();
+    clear();
 })
