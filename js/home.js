5673ad3580c8a6cb0b86fca4c7bac8c69e905b7c
guiazevedo17 <gui.azevedo@ymail.com>
2022-12-02 02:07:54 -0300

    tela perfil de cadastro concluida

---
+++ js/home.js
@@ -0,0 +1,10 @@
+const popup = document.querySelector(".popup-wrapper");
+const close = document.querySelector(".popup-close");
+
+function openSignUp(){
+    popup.style.display = 'flex'
+}
+
+function closeSignUp(){
+    popup.style.display = 'none'
+}
