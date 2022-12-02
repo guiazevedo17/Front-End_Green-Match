51d7df01dfb0df95176e0f53df2e0b72261f0aab
Guilherme Azevedo <guiazevedo@MacBook-Air-de-Guilherme.local>
2022-12-02 11:27:24 -0300

    inicio novas telas de cadastro

--- js/home.js
+++ js/home.js
@@ -1,10 +1,41 @@
 const popup = document.querySelector(".popup-wrapper");
 const close = document.querySelector(".popup-close");
+const popupSignup = document.querySelector(".signup");
+const popupSignupCustomer = document.querySelector(".signupCustomer");
+const popupSignupScavenger = document.querySelector(".signupScavenger");
 
-function openSignUp(){
-    popup.style.display = 'flex'
+function openSignUp() {
+  popup.style.display = "flex";
+  popupSignup.style.display = "flex";
 }
 
-function closeSignUp(){
-    popup.style.display = 'none'
+function openSignUpCustomer() {
+    popupSignup.style.display = "none";
+    popupSignupCustomer.style.display = "flex";
 }
+
+function openSignUpScavenger() {
+    popupSignup.style.display = "none";
+    popupSignupScavenger.style.display = "flex";
+}
+
+function returnSignUp() {
+    popupSignup.style.display = "flex";
+    popupSignupCustomer.style.display = "none";
+    popupSignupScavenger.style.display = "none";
+}
+
+popup.addEventListener("click", (event) => {
+  const classNameOfClickedElement = event.target.classList[0];
+  const classNames = ["popup-close", "popup-link", "popup-wrapper"];
+  const shouldClosePopup = classNames.some(
+    (className) => className === classNameOfClickedElement
+  );
+
+  if (shouldClosePopup) {
+    popup.style.display = "none";
+    popupSignup.style.display = "none";
+    popupSignupCustomer.style.display = "none";
+    popupSignupScavenger.style.display = "none";
+  }
+});
